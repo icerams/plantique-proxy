3e15bc800650d341e712393dcc48cc85c9222f21
icerams <crucio003@gmail.com>
2026-04-19 15:27:46 -0700

    Update identify.js

--- api/identify.js
+++ api/identify.js
@@ -1,15 +1,18 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
   const PLANTNET_KEY = process.env.PLANTNET_API_KEY;
+
   const response = await fetch(
     `https://my-api.plantnet.org/v2/identify/all?api-key=${PLANTNET_KEY}&lang=en&nb-results=5`,
     { method: "POST", body: req.body, headers: { "content-type": req.headers["content-type"] } }
   );
+
   const data = await response.json();
   res.status(response.status).json(data);
 }
